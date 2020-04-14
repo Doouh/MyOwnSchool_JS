2593d3cde0634a4d8247d529192f5d97ebd891b4
Doouh <ake.giraldo@hotmail.com>
2020-04-13 19:02:10 -0500

    Task 4 was complete

---
+++ 4-celsius_to_fahrenheit.js
@@ -0,0 +1,4 @@
+// Program that takes inputs from users in Celsius unit and returns it in Fahrenheit degree.
+let temp_cels = Number(prompt("Enter temp in Celsius:"));
+let temp_farh = temp_cels * 9/5 + 32;
+alert(`Temp in Farhenheit: ${temp_farh}`);
