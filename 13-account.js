b84e706816eed647abb61976ebbb5906cd49016d
Doouh <ake.giraldo@hotmail.com>
2020-04-22 10:03:44 -0500

    Task 13 was complete

---
+++ 13-account.js
@@ -0,0 +1,17 @@
+//Build a bank account is still defined by:
+//A name property.
+//A balance property, initially set to 0.
+//A credit method adding the value passed as an argument to the account balance.
+//A describe method returning the account description.
+class Account{
+	constructor (name){
+		this.name = name;
+		this.balance = 0;
+	}
+	credit(value){
+		this.balance += value;
+	}
+  	describe(){
+		return "Name: " + this.name + "\nBalance: " + this.balance;
+	}
+}
